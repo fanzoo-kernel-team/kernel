8d952aad68d063c5f2f1f93c3146913517b5f134
Bill Wheelock <mrbildo@mrbildo.net>
2023-03-01 12:17:09 -0500

    Update ValidationTests.cs

--- test/Fanzoo.Kernel.Testing.Unit/ValidationTests.cs
+++ test/Fanzoo.Kernel.Testing.Unit/ValidationTests.cs
@@ -11,5 +11,16 @@ namespace Fanzoo.Kernel.Tests
 
             Assert.False(result);
         }
+
+        [Fact]
+        public void Test_Can_Cast_Bool_To_Check()
+        {
+            Check check = true;
+
+            Assert.True(check.Result);
+
+            //cannot operate on Check that's been created from bool cast
+            Assert.Throws<InvalidOperationException>(() => check.IsNull<string?>(null));
+        }
     }
 }
