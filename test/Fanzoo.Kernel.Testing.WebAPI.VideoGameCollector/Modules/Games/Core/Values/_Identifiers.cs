481de780c2526a53b66a6f98e942fec26891329c
Bill Wheelock <mrbildo@mrbildo.net>
2023-03-24 11:44:04 -0400

    moved to public constructors with guids (#18)

--- test/Fanzoo.Kernel.Testing.WebAPI.VideoGameCollector/Modules/Games/Core/Values/_Identifiers.cs
+++ test/Fanzoo.Kernel.Testing.WebAPI.VideoGameCollector/Modules/Games/Core/Values/_Identifiers.cs
@@ -1,4 +1,9 @@
 ﻿namespace Fanzoo.Kernel.Testing.WebAPI.VideoGameCollector.Modules.Games.Core.Values
 {
-    public class GameIdentifierValue : GuidIdentifierValue<GameIdentifierValue> { }
+    public class GameIdentifierValue : GuidIdentifierValue<GameIdentifierValue>
+    {
+        public GameIdentifierValue() : base() { }
+
+        public GameIdentifierValue(Guid id) : base(id) { }
+    }
 }
