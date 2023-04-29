53ff2851cf531e54c3f6b49aa6bb3bc7c67c14c2
Bill Wheelock <mrbildo@mrbildo.net>
2023-04-29 00:57:58 -0400

    new extension methods
    * StopOnEmpty - automatically adds Cascade.Stop and NotEmpty()
    * MustWhenNotNull - appends a When checking for null of various types

---
+++ src/Fanzoo.Kernel/Web/Validation/FluentValidationExtensions.cs
@@ -0,0 +1,55 @@
+﻿namespace FluentValidation
+{
+    public static class FluentValidationExtensions
+    {
+        public static IRuleBuilderOptions<T, TProperty> StopOnEmpty<T, TProperty>(this IRuleBuilderInitial<T, TProperty> ruleBuilder) => ruleBuilder
+                .Cascade(CascadeMode.Stop)
+                    .NotEmpty();
+
+        public static IRuleBuilderOptions<T, TProperty> MustWhenNotNull<T, TProperty>(this IRuleBuilder<T, TProperty> ruleBuilder, Func<TProperty, bool> predicate)
+        {
+            var rule = ruleBuilder.Must(predicate);
+
+            switch (typeof(TProperty))
+            {
+                case Type type when type == typeof(string):
+                    rule.When(p => (p as string).IsNotNullOrWhitespace());
+                    break;
+
+                case Type type when type == typeof(Guid):
+                    rule.When(p => p != null && !p.Equals(Guid.Empty));
+                    break;
+
+                case Type type when type == typeof(int):
+                    rule.When(p => p != null && !p.Equals(0));
+                    break;
+
+                case Type type when type == typeof(long):
+                    rule.When(p => p != null && !p.Equals(0));
+                    break;
+
+                case Type type when type == typeof(decimal):
+                    rule.When(p => p != null && !p.Equals(0));
+                    break;
+
+                case Type type when type == typeof(DateTime):
+                    rule.When(p => p != null && !p.Equals(DateTime.MinValue));
+                    break;
+
+                case Type type when type == typeof(DateTimeOffset):
+                    rule.When(p => p != null && !p.Equals(DateTimeOffset.MinValue));
+                    break;
+
+                case Type type when type == typeof(TimeSpan):
+                    rule.When(p => p != null && !p.Equals(TimeSpan.Zero));
+                    break;
+
+                default:
+                    rule.When(p => p != null);
+                    break;
+            }
+
+            return rule;
+        }
+    }
+}
