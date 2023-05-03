35b923ab5761659cfe6923f00f612b2b1e58f1a1
Bill Wheelock <mrbildo@mrbildo.net>
2023-05-03 08:27:38 -0400

    added domaineventhandler (#39)

---
+++ src/Fanzoo.Kernel/Events/Domain/Abstractions/DomainEventHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Fanzoo.Kernel.Events.Domain
+{
+    public interface IDomainEventHandler<in TEvent> : IEventHandler<TEvent> where TEvent : IEvent
+    {
+
+    }
+
+    public abstract class DomainEventHandler<TEvent> : EventHandler<TEvent>, IDomainEventHandler<TEvent> where TEvent : IEvent
+    {
+        protected override ValueTask OnErrorAsync(Exception e) => throw e;
+    }
+}
