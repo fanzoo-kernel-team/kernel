9134dfdbd02cb7f16a54b6450645aae005d1804e
Bill Wheelock <mrbildo@mrbildo.net>
2023-05-25 10:52:02 -0400

    RazorPages Update
    * reorganized folders
    * added QueryablePageModel

---
+++ src/Fanzoo.Kernel/Web/Mvc/RazorPages/Abstractions/QueryablePageModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using KernelQueries = Fanzoo.Kernel.Queries;
+
+namespace Fanzoo.Kernel.Web.Mvc.RazorPages
+{
+    public abstract class QueryablePageModel : PageModel
+    {
+        protected QueryablePageModel(KernelQueries.QueryDispatcher queryDispatcher)
+        {
+            QueryDispatcher = queryDispatcher;
+        }
+
+        protected KernelQueries.QueryDispatcher QueryDispatcher { get; }
+
+        protected async ValueTask<TResult> DispatchQueryAsync<TResult>(KernelQueries.IQuery query)
+        {
+            var result = await QueryDispatcher.DispatchAsync<TResult>(query);
+
+            return result.IsSuccessful is false ? throw new InvalidOperationException(result.Error) : result.Value;
+        }
+
+        protected async ValueTask<TMappedResult> DispatchQueryAndMapAsync<TQueryResult, TMappedResult>(KernelQueries.IQuery query, Func<TQueryResult, TMappedResult> map)
+        {
+            var result = await QueryDispatcher.DispatchAsync<TQueryResult>(query);
+
+            return result.IsSuccessful is false ? throw new InvalidOperationException(result.Error) : map(result.Value);
+        }
+
+        protected async ValueTask<IEnumerable<TMappedResult>> DispatchQueryAndMapManyAsync<TQueryResult, TMappedResult>(KernelQueries.IQuery query, Func<IEnumerable<TQueryResult>, IEnumerable<TMappedResult>> map)
+        {
+            var result = await QueryDispatcher.DispatchAsync<IEnumerable<TQueryResult>>(query);
+
+            return result.IsSuccessful is false ? throw new InvalidOperationException(result.Error) : map(result.Value);
+        }
+    }
+}
