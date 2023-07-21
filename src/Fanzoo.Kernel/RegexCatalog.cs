5afd83c65b14e76c08b16764af0a90fb1769c699
Bill Wheelock <mrbildo@mrbildo.net>
2023-07-21 14:27:50 -0400

    fixed issue with port being part of IP (#58)

--- src/Fanzoo.Kernel/RegexCatalog.cs
+++ src/Fanzoo.Kernel/RegexCatalog.cs
@@ -8,7 +8,7 @@
         [GeneratedRegex(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$")]
         public static partial Regex Phone();
 
-        [GeneratedRegex(@"^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$")]
+        [GeneratedRegex(@"^(\b25[0-5]|\b2[0-4][0-9]|\b[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}(:\d+)?$")]
         public static partial Regex IPAddressPattern();
 
         [GeneratedRegex(@"^\d{5}(?:[-\s]\d{4})?$")]
